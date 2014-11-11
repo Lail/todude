5b4cddc6f244555e265e3b977946883a6ab7a10d
Lail Brown <lail@luxmassive.com>
2014-11-11 15:01:45 -0500

    Update for FactoryGirl

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,30 +2,30 @@ require "rails_helper"
 
 RSpec.describe Project, :type => :model do
   it "can create a valid instance" do
-    my_project = Project.create!(name: "My first project", color: Project::COLORS.sample)
-    expect(my_project).to be_valid
+  	project = create :project
+    expect(project).to be_valid
   end
 
   # Validations
   it "requires a name" do
-    my_project = Project.create(name: nil, color: Project::COLORS.sample)
-    expect(my_project).to be_invalid
+    project = build :project, name: nil
+    expect(project).to be_invalid
   end
 
   it "requires a name with a reasonable length" do
     too_long = 'X' * 501
-    my_project = Project.create(name: too_long, color: Project::COLORS.sample)
-    expect(my_project).to be_invalid
+    project = build :project, name: too_long
+    expect(project).to be_invalid
   end
 
   it "requires a color" do
-    my_project = Project.create(name: "My first project", color: nil)
-    expect(my_project).to be_invalid
+    project = build :project, color: nil
+    expect(project).to be_invalid
   end
 
   it "requires a valid color" do
-    my_project = Project.create(name: "My first project", color: "000000")
-    expect(my_project).to be_invalid
+    project = build :project, color: "000000"
+    expect(project).to be_invalid
   end
 
 end
